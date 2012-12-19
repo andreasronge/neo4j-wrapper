8c8a767c138f61a258deefbd3240dcabc702e6d6
Eric Pierce <wapcaplet88@gmail.com>
2012-12-19 11:27:12 -0700

    Convert DateTimes to UTC
    In DateTimeConverter#to_java, if the given value is a DateTime, it is
    automatically converted to UTC using DateTime#new_offset(0). Date and
    Time objects are left alone, since they don't respond to new_offset.
    
    Spec tests included.

--- spec/neo4j/type_converters/datetime_converter_spec.rb
+++ spec/neo4j/type_converters/datetime_converter_spec.rb
@@ -33,6 +33,11 @@ describe "Date and Time converters" do
 
 
   describe "DateTime" do
+    before(:each) do
+      @dt = 1352538487
+      @hr = 3600
+    end
+
     subject { Neo4j::TypeConverters::DateTimeConverter }
 
     its(:convert?, DateTime)  { should be_true }
@@ -42,7 +47,14 @@ describe "Date and Time converters" do
 
     its(:to_java, nil)        { should be_nil }
     its(:to_ruby, nil)        { should be_nil }
-  end
 
+    its(:to_java, DateTime.parse("2012-11-10T09:08:07-06:00")) { should === @dt + 6*@hr }
+    its(:to_java, DateTime.parse("2012-11-10T09:08:07-04:00")) { should === @dt + 4*@hr }
+    its(:to_java, DateTime.parse("2012-11-10T09:08:07-02:00")) { should === @dt + 2*@hr }
+    its(:to_java, DateTime.parse("2012-11-10T09:08:07+00:00")) { should === @dt }
+    its(:to_java, DateTime.parse("2012-11-10T09:08:07+02:00")) { should === @dt - 2*@hr }
+    its(:to_java, DateTime.parse("2012-11-10T09:08:07+04:00")) { should === @dt - 4*@hr }
+    its(:to_java, DateTime.parse("2012-11-10T09:08:07+06:00")) { should === @dt - 6*@hr }
+  end
 
 end
